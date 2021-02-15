629559cddc399c0d4a095ed90eeaf5e0f2ef2320
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-15 16:29:58 +1000

    Add files via upload

---
+++ WorkshopExercises/Wk7_WebDocuments/4-web_document_copier.py
@@ -0,0 +1,80 @@
+#-----------------------------------------------------------
+#
+# Web Document Copier
+#
+# The Python script below reads the contents of a web
+# document, the Wikipedia home page, and prints it to the
+# shell window.  Since Tk windows cannot handle all Unicode
+# characters, it converts the text to an equivalent ASCII
+# character string before printing it.
+#
+# To make this program more useful, modify it in two
+# ways:
+#
+# 1) Instead of hardwiring the web document's address in the
+#    Python code, prompt the user for the address of the web
+#    document they want to copy.
+#
+# 2) Instead of printing the contents of the web document
+#    to the shell window, write them to an HTML file, so
+#    that you have a permanent copy of the document.  In this
+#    situation you should save the text as Unicode rather
+#    than ASCII.  (However, if you view the document in a web
+#    browser you may be disappointed to discover that some of
+#    the links to images and other "external" document elements
+#    no longer work properly, depending on whether the URLs
+#    involved are relative or absolute.)
+#
+# 3) Once your "copier" is working, use it to download a number
+#    of web pages to your computer, then open the files in a
+#    text editor or a web browser.  Examine the document's
+#    source code and ensure that you can identify the main
+#    elements in the document such as the head and body
+#    sections, the document title, headings within the body,
+#    etc.
+#
+# NB: If you examine the downloaded web pages in a web browser
+# you may find that some hyperlinks are broken.  This can
+# happen when the HTML source code contains links relative to
+# current file, rather than using a full URL address.  By
+# copying the code to a new place, but not adjusting the
+# relative links accordingly, we can "break" them.
+# 
+
+from urllib.request import urlopen
+
+# This is the web page that will be downloaded
+# 1) Instead of hardwiring the web document's address in the
+#    Python code, prompt the user for the address of the web
+#    document they want to copy.
+web_address = input('Enter web address to copy: ')
+##url = 'http://www.wikipedia.org/'
+url = web_address
+
+# Read the contents of the web page as a character string,
+# suitable for printing to an ASCII-only Tk widget, using
+# backslashes to "escape" any non-ASCII characters
+
+# 2) Instead of printing the contents of the web document
+#    to the shell window, write them to an HTML file, so
+#    that you have a permanent copy of the document.  In this
+#    situation you should save the text as Unicode rather
+#    than ASCII.  (However, if you view the document in a web
+#    browser you may be disappointed to discover that some of
+#    the links to images and other "external" document elements
+#    no longer work properly, depending on whether the URLs
+#    involved are relative or absolute.)
+web_page = urlopen(url)
+web_page_bytes = web_page.read()
+web_page_text = web_page_bytes.decode('UTF-8', 'backslashreplace')
+
+#open a file to save web page to
+html_file = open('dummyfile.html', 'w', encoding = "UTF-8")
+html_file.write(web_page_text)
+html_file.close()
+
+# Print the downloaded web page source code in IDLE's
+# shell window (which is a Tk GUI widget)
+##print(web_page_text)
+
+
